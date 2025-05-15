eb5979a676ce34264a7150c935b40d0e971f88b0
SHAN-E-ALI SHAH <shanealisa456@gmail.com>
2025-05-15 16:45:17 +0500

    change backendlink for deployment

--- Frontend/src/Lib/axios.js
+++ Frontend/src/Lib/axios.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export const axiosInstance = axios.create({
     baseURL: import.meta.env.MODE === "development"
         ? "http://localhost:5000/api"
-        : "/api",
+        : import.meta.env.VITE_API_URL,  // Use full Render backend URL
     withCredentials: true
 });
